b0e23256143633050cce1fc425da48db5da684f3
Melria <127540628+Melria@users.noreply.github.com>
2024-09-12 11:09:10 +0100

    modified navbar + mesh animation added

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,7 +3,7 @@ import dashboard from '../assets/dashboard.jpg';
 
 export const  Home=()=>{
   return (
-    <div className='home' id="home">
+    <div className='home mesh-item' id="home">
         <h1>Make Your First <span>Impression</span> Count</h1>
         <p className='homeText'>powerful seal-serve product analytics to help you convert<br/>
         engage and retain  more users </p>
